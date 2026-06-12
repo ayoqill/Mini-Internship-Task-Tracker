b64edd77a49b77efef7c16f2fdbfb1396bb012cd
ayoqill <aqilaiman058@gmail.com>
2026-06-12 13:00:49 +0800

    setup and HELLO VUE

---
+++ mini/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello Vue')
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+  </main>
+</template>
+
+<style scoped>
+main {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
